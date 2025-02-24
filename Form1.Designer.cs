b4d71394a032364107b14e6f29a0f2b7b31a502d
momocntt <nguyenthihoaimo1207@gmail.com>
2025-02-24 09:19:41 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace bai12_Listbox_Name
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstName = new System.Windows.Forms.ListBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtSelect = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(41, 47);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(179, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(273, 67);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 45);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstName
+            // 
+            this.lstName.FormattingEnabled = true;
+            this.lstName.Location = new System.Drawing.Point(41, 94);
+            this.lstName.Name = "lstName";
+            this.lstName.Size = new System.Drawing.Size(179, 212);
+            this.lstName.TabIndex = 2;
+            this.lstName.SelectedIndexChanged += new System.EventHandler(this.lstName_SelectedIndexChanged);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(273, 168);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(100, 45);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(273, 261);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(100, 45);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(273, 337);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(100, 43);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtSelect
+            // 
+            this.txtSelect.Location = new System.Drawing.Point(289, 407);
+            this.txtSelect.Name = "txtSelect";
+            this.txtSelect.Size = new System.Drawing.Size(72, 20);
+            this.txtSelect.TabIndex = 0;
+            this.txtSelect.Text = "2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 450);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.lstName);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSelect);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstName;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtSelect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai12_Listbox_Name
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstName.Items.Add(txtName.Text);
+            txtName.Text = "";
+            txtName.Focus();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+            if (lstName.SelectedIndex < 0)
+            {
+                MessageBox.Show("Hãy chọn tên trong danh sách", "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else
+            {
+                lstName.Items[lstName.SelectedIndex] = txtName.Text;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstName.Items.Add("củ chi");
+            lstName.Items.Add("củ chi");
+            lstName.Items.Add("củ chi");
+            lstName.Items.Add("củ chi");
+            lstName.Items.Add("củ chi");
+            lstName.Items.Add("củ chi");
+            lstName.Items.Add("củ chi");
+        }
+
+        private void lstName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstName.SelectedIndex < 0)
+            {
+                return;
+            }
+            txtName.Text = lstName.Items[lstName.SelectedIndex].ToString();
+            }
+        
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstName.SelectedIndex > 0)
+            {
+                lstName.Items.RemoveAt(lstName.SelectedIndex);
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            lstName.Items.Insert(Convert.ToInt32(txtSelect.Text), txtName.Text);
+        }
+    }
+}
+    
